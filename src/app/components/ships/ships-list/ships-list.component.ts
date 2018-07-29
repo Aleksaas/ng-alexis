e6b6053e6f69e3eba8bb26b011742c4fe5e2286d
Aleksa Stancetic <aleksaas89@gmail.com>
2018-07-29 19:48:51 +0200

    - Merged

--- src/app/components/ships/ships-list/ships-list.component.ts
+++ src/app/components/ships/ships-list/ships-list.component.ts
@@ -1,7 +1,10 @@
+import { SearchRequest } from './../../../model/search.model';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map } from 'rxjs/operators';
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { ShipService } from '@app/services/ship.service';
-import { Observable } from 'rxjs';
-import { ShipDetails } from '@app/model/ship.model';
+import { Observable, Subject } from 'rxjs';
+import { ApiResponse } from '@app/model/common.model';
 
 @Component({
     selector: 'ships-list',
@@ -11,11 +14,36 @@ import { ShipDetails } from '@app/model/ship.model';
 export class ShipsListComponent implements OnInit {
 
     constructor(
-        private shipService: ShipService
+        private shipService: ShipService,
+        private http: HttpClient
     ) { }
+
+    ships$: Observable<any>
+
+    ships: any
+
+    shipsSub$: Subject<any> = new Subject()
 
     ngOnInit() {
 
         this.shipService.loadSideNavigationShips();
+
+        this.ships$ = this.http
+            .post<ApiResponse<any>>(`http://localhost:47858/api/ships/search`, new SearchRequest())
+
+        // this.ships$.subscribe(response => {
+        //     this.ships = response;
+        // });
+
+        this.shipsSub$.subscribe(response => {
+            this.ships = response.data.result[0].id;
+        });
+    }
+
+    async reload() {
+        const response = await this.http
+            .post<ApiResponse<any>>(`http://localhost:47858/api/ships/search`, new SearchRequest()).toPromise()
+
+        this.shipsSub$.next(response)
     }
 }
