b1229f948c4ab4fd85f0c0fc23bd0bf7243130d0
Aleksa Stancetic <aleksa.stancetic@nortal.com>
2019-06-27 16:25:40 +0200

    - Search changes

--- src/app/components/customers/customers-list/customers-list.component.ts
+++ src/app/components/customers/customers-list/customers-list.component.ts
@@ -4,13 +4,11 @@ import { Component, OnInit } from '@angular/core';
 import { SearchResponse, SearchRequest } from '@app/model/common.model';
 
 @Component({
-    selector: 'app-customers-list',
+    selector: 'customers-list',
     templateUrl: './customers-list.component.html',
     styleUrls: ['./customers-list.component.css']
 })
 export class CustomersListComponent implements OnInit {
-
-    customersSearchResponse: SearchResponse<CustomerDetails> = new SearchResponse<CustomerDetails>();
 
     constructor(private customerService: CustomersService) { }
 
@@ -18,19 +16,11 @@ export class CustomersListComponent implements OnInit {
         this.setPage({ offset: 0 });
     }
 
-    /**
-    * Populate the table with new data based on the page number
-    * @param page The page to select
-    */
-    async setPage(pageInfo: any) {
-        const request = new SearchRequest<CustomerQuery>();
-
-        request.pageNumber = pageInfo.offset;
-
-        this.customersSearchResponse = await this.customerService.searchCustomers(request);
+    setPage(pageInfo: any) {
+        this.customerService.setCurrentPage(pageInfo.offset);
     }
 
-    onSort($event: any) {
+    setSort($event: any) {
         console.log($event);
     }
 
--- src/app/components/customers/customers.service.ts
+++ src/app/components/customers/customers.service.ts
@@ -1,4 +1,4 @@
-import { CustomerDetails, CustomerQuery } from './../../model/api.model';
+import { CustomerDetails, CustomerQuery } from '@app/model/api.model';
 import { CustomersApiService } from '@app/services/api/customers.api.service';
 import { SearchRequest, SearchResponse } from '@app/model/common.model';
 import { Injectable } from '@angular/core';
@@ -6,16 +6,25 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class CustomersService {
 
-    customers: CustomerDetails[];
+    customerSearchRequest: SearchRequest<CustomerQuery> = new SearchRequest<CustomerQuery>();
+
+    customersSearchResponse: SearchResponse<CustomerDetails> = new SearchResponse<CustomerDetails>();
 
     constructor(private customersApiService: CustomersApiService) {
 
     }
 
-    async searchCustomers(request?: SearchRequest<CustomerQuery>): Promise<SearchResponse<CustomerDetails>> {
+    async setCurrentPage(page: number) {
+        this.customerSearchRequest.pageNumber = page;
+        await this.searchCustomers();
+    }
 
-        const response = await this.customersApiService.searchCustomers(request);
+    async setSearchQuery(query: CustomerQuery) {
+        this.customerSearchRequest.query = query;
+        await this.searchCustomers();
+    }
 
-        return response;
+    private async searchCustomers() {
+        this.customersSearchResponse = await this.customersApiService.searchCustomers(this.customerSearchRequest);
     }
 }
